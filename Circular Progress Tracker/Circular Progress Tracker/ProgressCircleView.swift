f3ed65e3ab5fc0fb5ce45bd6465af5c4b642b168
Kayden Thomson <kaydenthomson@gmail.com>
2017-06-23 13:48:14 -0600

    Styled. Added progress percentage label.

--- Circular Progress Tracker/Circular Progress Tracker/ProgressCircleView.swift
+++ Circular Progress Tracker/Circular Progress Tracker/ProgressCircleView.swift
@@ -15,10 +15,30 @@ class ProgressCircleView: UIView {
     var scale: CGFloat = 0.9
     
     @IBInspectable
-    var circleWidth: CGFloat = 20
+    var lineWidth: CGFloat = 20
     
     @IBInspectable
     var containerCircleColor: UIColor = UIColor.darkGray
+    
+    @IBInspectable
+    var animatedCircleColor: UIColor = UIColor.blue
+    
+    @IBInspectable
+    var animationDuration: Double = 1.5
+    
+    var completion: CGFloat = 1
+    
+    private let shapeLayer = CAShapeLayer()
+    
+    private var animationStartAngle: CGFloat = 3 * CGFloat.pi / 2
+    
+    private var animationEndAngle: CGFloat {
+        if (completion >= 1) {
+            return CGFloat(359 - 90).degreesToRadians
+        }
+        let degrees = completion * 360  - 90
+        return degrees.degreesToRadians
+    }
     
     private var circleRadius: CGFloat {
         return min(bounds.size.width, bounds.size.height) / 2 * scale
@@ -30,12 +50,35 @@ class ProgressCircleView: UIView {
 
     private func pathForCircle(startAngle start: CGFloat, endAngle end: CGFloat) -> UIBezierPath {
         let path = UIBezierPath(arcCenter: circleCenter, radius: circleRadius, startAngle: start, endAngle: end, clockwise: true)
-        path.lineWidth = circleWidth
+        path.lineWidth = lineWidth
         return path
+    }
+    
+    private func animateCircle() {
+        shapeLayer.strokeColor = animatedCircleColor.cgColor
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.lineWidth = lineWidth
+        shapeLayer.lineCap = kCALineCapRound
+        self.layer.addSublayer(shapeLayer)
+        shapeLayer.path = pathForCircle(startAngle: animationStartAngle, endAngle: animationEndAngle).cgPath
+        
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.fromValue = 0.0
+        animation.toValue = 1.0
+        animation.duration = animationDuration
+        shapeLayer.add(animation, forKey: "drawLineAnimation")
     }
     
     override func draw(_ rect: CGRect) {
         containerCircleColor.set()
         pathForCircle(startAngle: 0, endAngle: 2*CGFloat.pi).stroke()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.animateCircle()
+        }
     }
+}
+
+extension CGFloat {
+    var degreesToRadians: CGFloat { return self * .pi / 180 }
+    var radiansToDegrees: CGFloat { return self * 180 / .pi }
 }
--- Circular Progress Tracker/Circular Progress Tracker/ViewController.swift
+++ Circular Progress Tracker/Circular Progress Tracker/ViewController.swift
@@ -9,17 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var completion: Double = 0.834
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return .lightContent
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBOutlet weak var progressLabel: UILabel!{
+        didSet {
+            let text: String = String(format: "%.0f", completion * 100)
+            let decimalText: String = String(format: "%.1f", completion * 100)
+            
+            if (text + ".0" == decimalText) {
+                progressLabel.text = text + "%"
+            } else {
+                progressLabel.text = decimalText + "%"
+            }
+        }
     }
-
-
+    
+    @IBOutlet weak var progressCircleView: ProgressCircleView! {
+        didSet {
+            progressCircleView.completion = CGFloat(self.completion)
+        }
+    }
 }
 
