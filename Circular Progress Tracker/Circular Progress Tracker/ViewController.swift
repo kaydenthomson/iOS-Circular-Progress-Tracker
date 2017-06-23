5d12a785b8f47c364ffefa48a99d0490750c6a92
Kayden Thomson <kaydenthomson@gmail.com>
2017-06-23 08:58:00 -0600

    init

---
+++ Circular Progress Tracker/Circular Progress Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Circular Progress Tracker
+//
+//  Created by Kayden Thomson on 6/23/17.
+//  Copyright © 2017 Kayden Thomson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
